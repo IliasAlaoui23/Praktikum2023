3c75fc9f5e6d319ea680d40ef83155cbb3c5c909
IliasAlaoui23 <123734632+IliasAlaoui23@users.noreply.github.com>
2023-02-22 13:30:31 +0000

    Test2.0

--- src/Array/bsp.java
+++ src/Array/bsp.java
@@ -1,5 +1,5 @@
 public class bsp {
-    public static void main(String[] arg) {
-        System.out.println(" bsp arrays nicht verhanden  ");
-    } 
+    public static void public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
