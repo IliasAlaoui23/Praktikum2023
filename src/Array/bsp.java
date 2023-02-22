e698e385223f3876fe439664c6792f4f705187e5
IliasAlaoui23 <123734632+IliasAlaoui23@users.noreply.github.com>
2023-02-22 13:33:01 +0000

    Correct

--- src/Array/bsp.java
+++ src/Array/bsp.java
@@ -1,5 +1,5 @@
 public class bsp {
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
